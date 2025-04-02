baa5828d667926057a680039fb6906bebc88d4bb
LehaBruev <115392339+LehaBruev@users.noreply.github.com>
2025-04-02 10:07:27 +0800

    Текстуры перевел на новый класс

--- Havier Than Air S/TextureManager.cs
+++ Havier Than Air S/TextureManager.cs
@@ -8,9 +8,40 @@ using SFML.System;
 
 namespace Havier_Than_Air_S
 {
-    internal class TextureManager
+    public class TextureManager
     {
 
+        //Фон
+        Texture backgroundLevel1;
+        Texture backgroundLevel3;
+
+        //Техника
+        Texture uh61;
+        Texture aiming;
+
+        //Меню
+        Texture maimenutexture;
+        Texture scoresprite;
+        
+
+        public TextureManager()
+        {
+
+             backgroundLevel1 = new Texture("BackGroundLevel1.png");
+             backgroundLevel3 = new Texture("BackGroundLevel3.png"); 
+
+            //Техника
+             uh61 = new Texture("uh61all.png");
+             aiming = new Texture("aim.png");
+
+            //Меню
+             maimenutexture = new Texture("mainmenu.png");
+             scoresprite = new Texture("score1back.png");
+
+
+
+
+        }
 
         /*
         //ТЕКСТУРЫ
