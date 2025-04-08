0ea14ea6d9d257df3534a59ace876bca020d0829
LehaBruev <115392339+LehaBruev@users.noreply.github.com>
2025-04-08 09:07:14 +0800

    Открыл новую ветку, чтобы отработать NR

--- Havier Than Air S/Hely.cs
+++ Havier Than Air S/Hely.cs
@@ -11,28 +11,27 @@ namespace Havier_Than_Air_S
 
         //Настройки верталета
 
-        static float helilife = 100;// жизни
-        static float manageability = 5;// управляемость
-        static float helilifemax = 300;// максимальные жизни Вертолета
-        static float helienginelife = 100; //исправность двигателя Вертолета
+        //Переменные
+        static float helilifeCurrent = 200;// жизни
         static int engineswitch = 1; // включение двигателя
         static int autopilotswitch = 0; // автопилот горизонтальный, удерживает угол в точке 0 градусов
-        static float fuelrashod = 1; // расход топлива
-
         static float altitude = 200; // Высота
-
-        static float maxangle = 65; // Максимальный угол атаки
         static float helifuel = 950; // Топливо в баках
-        static float helifuelmax = 1300; // Максимальное топливо в баках
         static int bang1 = 1;
+        static int gunmode = 0;
 
+
+        //Характеристики
+        static float helilifemax = 300;// максимальные жизни Вертолета
+        static float helienginelife = 100; //исправность двигателя Вертолета
+        static float fuelrashod = 1; // расход топлива
+        static float manageability = 5;// управляемость
+        static float maxangle = 65; // Максимальный угол атаки
+        static float helifuelmax = 1300; // Максимальное топливо в баках
         static float maxboost = 11250; // максимальное ускорение от двигателя
 
-
-
-
-        //ВООРУЖЕНИЕ ВЕРТАЛЕА
-        static int gunmode = 0;
+        
+        
 
         // NR ракеты МОДЕ 2
         static float NRrocketlenght = 20;
