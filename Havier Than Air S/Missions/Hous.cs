86657a1e6cfd9b2389f28e6320d16491b3407277
LehaBruev <115392339+LehaBruev@users.noreply.github.com>
2025-05-13 17:31:18 +0800

    дома

---
+++ Havier Than Air S/Missions/Hous.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Graphics;
+
+namespace Havier_Than_Air_S.Missions
+{
+    public class Hous
+    {
+
+        public RectangleShape rectShape;
+
+        public Hous() 
+        { 
+        rectShape.FillColor = Color.White;
+        
+        
+        }
+
+
+
+
+
+    }
+}
